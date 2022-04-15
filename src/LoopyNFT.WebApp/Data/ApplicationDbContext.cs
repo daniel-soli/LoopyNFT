8fcca2b4e5c51364c0d9f58e641328d9c68a6a3a
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-04-15 11:24:55 +0200

    Add project files.

---
+++ src/LoopyNFT.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ElCamino.AspNetCore.Identity.AzureTable;
+using ElCamino.AspNetCore.Identity.AzureTable.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoopyNFT.WebApp.Data
+{
+    public class ApplicationDbContext : IdentityCloudContext
+    {
+        //public ApplicationDbContext() : base()
+        //{
+        //}
+
+        public ApplicationDbContext(IdentityConfiguration config) : base(config)
+        {
+
+        }
+    }
+}
---
+++ src/LoopyNFT.WebApp/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using ElCamino.AspNetCore.Identity.AzureTable.Model;
+
+namespace LoopyNFT.WebApp.Entities;
+public class AppUser : IdentityUser
+{
+    public string WalletAddress { get; set; }
+}
